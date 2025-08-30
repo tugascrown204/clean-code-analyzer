464688da8e574e782b70e69a5149b7f22a3bbed2
tugascrown204 <gajdertevet@hotmail.com>
2025-08-30 09:01:20 +0330

    Add install_requires with necessary dependencies

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='clean-code-analyzer',
     version='0.1',
     packages=find_packages(),
-    install_requires=[],
+    install_requires=['requests', 'pylint'],  # Added necessary dependencies
     entry_points={
         'console_scripts': [
             'clean-code-analyzer=analyzer:main',
